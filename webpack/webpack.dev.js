86a365812483e69d5d33fcdda0af7047bf6ec3d8
joaovitor32 <joaovitormunizlopes@gmail.com>
2021-09-06 22:33:34 -0300

    Changing webpack

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,22 +1,37 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-  name: "browser",
   entry: path.resolve(__dirname, "src/index.tsx"),
-  resolve: {
-    extensions: [".js", ".tsx", ".ts", ".json"],
-  },
   output: {
     path: path.resolve(__dirname, "dist"),
   },
+  extensions: [".js", ".jsx", ".json", ".ts", ".tsx"], // other stuff
   resolve: {
     modules: [".", "node_modules"],
   },
+  devServer: {
+    port: 3000,
+    watchContentBase: true,
+  },
+  fallback: {
+    fs: false,
+    tls: false,
+    net: false,
+    path: false,
+    zlib: false,
+    http: false,
+    https: false,
+    stream: false,
+    crypto: false,
+    "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
+  },
+
   module: {
     rules: [
       {
-        test: /\.?ts$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -30,32 +45,13 @@ module.exports = {
         },
       },
       {
-        test: /\.?tsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              ["@babel/preset-env", { targets: { node: "current" } }],
-              "@babel/preset-typescript",
-              "@babel/preset-react",
-            ],
-          },
-        },
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new NodePolyfillPlugin(),
   ],
 };
